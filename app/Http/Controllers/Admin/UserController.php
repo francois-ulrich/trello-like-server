2646cc0d6d58c3ddc99c1d4f3b0212cb293fb2ed
François Ulrich <fulrich3@gmail.com>
2026-01-23 15:00:34 +0100

    added profile & role load to user controller GET methods

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::all();
+        $users = User::with('profile', 'role')->get();
         return response()->json($users, 200);
     }
 
@@ -22,7 +22,7 @@ class UserController extends Controller
      */
     public function show(User $user)
     {
+        $user->load('profile', 'role');
         return response()->json($user, 200);
     }
-
 }
