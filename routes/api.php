ab5b60d6cf7fabcb440b491d8859dae3d5d2e97d
François Ulrich <fulrich3@gmail.com>
2026-03-27 10:03:29 +0100

    added password change email send route (auth/forgot-password) with email send

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use App\Http\Controllers\Admin\UserAdminController;
 use Illuminate\Auth\Middleware\EnsureEmailIsVerified;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
 
 Route::middleware(JwtMiddleware::class)->group(function () {
     // Authentication
@@ -21,9 +22,24 @@ Route::middleware(JwtMiddleware::class)->group(function () {
         Route::post('register', [AuthController::class, 'register'])->withoutMiddleware([JwtMiddleware::class]);
         Route::post('login', [AuthController::class, 'login'])->withoutMiddleware([JwtMiddleware::class]);
         Route::post('logout', [AuthController::class, 'logout'])->withoutMiddleware([JwtMiddleware::class]);
+        Route::post('forgot-password', function (Request $request) {
+            $request->validate(['email' => 'required|email']);
+
+            $status = Password::sendResetLink(
+                $request->only('email')
+            );
+
+            return $status === Password::ResetLinkSent
+                ? ApiResponse::success(['status' => __($status)], "Email has been sent !")
+                : ApiResponse::error("An error occured.");
+
+            return ApiResponse::success(null, "Email has been sent !");
+        })->withoutMiddleware([JwtMiddleware::class]);
+
         Route::get('me', [UserController::class, 'show'])->scopeBindings();
         Route::patch('me', [UserController::class, 'update'])->scopeBindings();
         Route::delete('me', [UserController::class, 'destroy'])->scopeBindings();
+
         Route::post('verification-notification', function (Request $request) {
             $request->user()->sendEmailVerificationNotification();
             return ApiResponse::success(null, "Email has been sent !");
